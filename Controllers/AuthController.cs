027664963d2710432ff3325ff4d0832709e54cc6
pffk21 <gerpaul588@gmail.com>
2026-08-28 11:31:13 +0300

    Add current user endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using CloneAmazonBack.DTOs.Auth;
 using CloneAmazonBack.Services.Interfaces;
 using Microsoft.AspNetCore.Authorization;
@@ -64,5 +65,31 @@ public class AuthController : ControllerBase
         {
             return Unauthorized(new { message = ex.Message });
         }
+    }
+
+    /// <summary>
+    /// Returns information about the currently authenticated user.
+    /// </summary>
+    /// <returns>Current user information from JWT claims.</returns>
+    [Authorize]
+    [HttpGet("me")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    public IActionResult Me()
+    {
+        var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var email = User.FindFirst(ClaimTypes.Email)?.Value;
+        var firstName = User.FindFirst(ClaimTypes.GivenName)?.Value;
+        var lastName = User.FindFirst(ClaimTypes.Surname)?.Value;
+        var role = User.FindFirst(ClaimTypes.Role)?.Value;
+
+        return Ok(new
+        {
+            Id = userId,
+            Email = email,
+            FirstName = firstName,
+            LastName = lastName,
+            Role = role
+        });
     }
 }
